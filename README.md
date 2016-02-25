4dfafb4cf173167709b7d4cf756636a6a53a20e9
Michael Borde <michael.borde@ubimail.fr>
2016-02-25 16:29:16 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+# js-angular1 [![Build Status](https://travis-ci.org/arpinum-oss/js-angular1.svg?branch=master)](https://travis-ci.org/arpinum-oss/js-angular1)
+
+**js-angular1** is a collection of simple Angular 1 reusable modules.
+
+## License
+
+[MIT](LICENSE)
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let path = require('path');
+let webpack = require('webpack');
+
+let config = {};
+
+config.entry = {
+  index: './lib/index.js'
+};
+
+config.output = {
+  path: path.join(__dirname, 'dist'),
+  filename: '[name].js',
+  library: '@arpinum/angular1',
+  libraryTarget: 'umd'
+};
+
+config.module = {
+  loaders: [{
+    test: /\.js$/,
+    loaders: ['ng-annotate', 'babel'],
+    exclude: [/node_modules/]
+  }]
+};
+
+config.plugins = [
+  new webpack.NoErrorsPlugin(),
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.UglifyJsPlugin()
+];
+
+module.exports = config;
